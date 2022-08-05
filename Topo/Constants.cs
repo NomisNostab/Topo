641b185e00673dba016864c392d570fed4aadbdb
simon <simon@batson.id.au>
2022-08-05 22:20:15 +1000

    Issue-121: Bump version

--- Topo/Constants.cs
+++ Topo/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string Version = "1.28";
+        public const string Version = "1.29";
 
         public enum OutputType
         {
